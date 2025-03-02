29100c7472407839cf501f8ad9f87fd4b46443e0
Arun Kumar <stockerunknown@gmail.com>
2025-03-02 23:34:37 +0530

    Clear the extra files and setup the workstation

---
+++ Practice1/Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [jokes, alljokes] = useState({})
+
+  return (
+    <div>
+      <h2>Jokes</h2>
+
+{
+
+
+
+  
+}
+
+
+
+    </div>
+  )
+}
+
+export default App

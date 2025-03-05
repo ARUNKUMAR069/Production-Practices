8b49d5d158f16658fc4688d74eb75ec95c77f2e8
Arun Kumar <stockerunknown@gmail.com>
2025-03-05 21:59:42 +0530

    Know about what is CORS Policy how to Use Proxy In Vite or Any toolchain And also how to fetch data from the api  and good pratices for Backend Integration

---
+++ Cors.md
@@ -0,0 +1,4 @@
+Why Cors What is the Work of CORS in Backend?
+Access to XMLHttpRequest at 'http://localhost:3000/jokes' from origin 'http://localhost:5173' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
+
--- Practice1/Backend/server.js
+++ Practice1/Backend/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+import cors from 'cors';
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+//  CORS POLICY
+app.use(cors());
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        { id: 1, title: "Science Joke", content: "Why don't scientists trust atoms? Because they make up everything!" },
+        { id: 2, title: "Scarecrow Joke", content: "Why did the scarecrow win an award? Because he was outstanding in his field!" },
+        { id: 3, title: "Skeleton Joke", content: "Why don't skeletons fight each other? They don't have the guts." }
+    ];
+    res.json(jokes);
 });
-
-
 
 
 app.listen(port, () => {

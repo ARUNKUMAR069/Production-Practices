d3fd2fba8c7f8c7a372a093ebf18753c3bd246f2
Arun Kumar <stockerunknown@gmail.com>
2025-03-07 00:30:58 +0530

    Learn to send json response

--- Practice2/index.js
+++ Practice2/index.js
@@ -3,6 +3,23 @@ const app = express();
 require('dotenv').config();
 
 
+const githubData = [{
+    name: 'Rahul',
+    age: 22
+},
+{
+    name: 'Rohit',
+    age: 23
+},
+{
+    name: 'Raj',
+    age: 24
+}
+]
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
@@ -16,7 +33,10 @@ app.get('/login', (req, res) => {
 app.get('/youtube', (req, res) => {
     res.send('<h1>Chai aur code</h1>');
 })
+app.get('/github', (req, res) => {
+    res.json(githubData);
+})
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 })

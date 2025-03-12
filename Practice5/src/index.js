ada5df46fd798f9c789cd2333dd2d90f7b096df3
Arun Kumar <stockerunknown@gmail.com>
2025-03-12 21:34:41 +0530

    Learn the proper way to connect the database

--- Practice5/src/index.js
+++ Practice5/src/index.js
@@ -1,41 +1,8 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
 
-const app = express();
-
-
-; (async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (err) => {
-            console.error("ERROR: Could not start server", err)
-            throw err
-        })
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-
-    }
-    catch (err) {
-        console.error(err)
-        console.error("ERROR: Could not connect to the database", error)
-        throw err
-    }
-})()
-    */
+connectDB();
